99c584ce59b7da965df5c83966234a048b114ab2
김준서 <rlawnstj545@naver.com>
2022-12-19 16:01:03 +0900

    로그인 되어있을시 login화면 접근하면 index로 돌려보냄

--- src/main/java/com/acorn/koala/Controller/LoginController.java
+++ src/main/java/com/acorn/koala/Controller/LoginController.java
@@ -24,6 +24,10 @@ public class LoginController {
 	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		if(session.getAttribute("id") != null) {
+			return "redirect:index";
+		}
 		return "login";
 	}
 	
@@ -33,37 +37,41 @@ public class LoginController {
 		response.setContentType("text/html;charset=utf-8");
 		
 		
-		LoginService service = new LoginService(dao);
-		int result = service.loginUser(id, pw);
+		
+	
+			LoginService service = new LoginService(dao);
+			int result = service.loginUser(id, pw);
 
-		
-		
-		if(result == 1 ){	
-			HttpSession session = request.getSession();
-			session.setAttribute("id", id);
-			session.setAttribute("type", login_type);
 			
-			if(rememberId) {
-				//쿠키생성, 저장
-				Cookie cookie = new Cookie("id",id);
-				response.addCookie(cookie);
-			} else {
-				//쿠키삭제
-				Cookie cookie = new Cookie("id",id);
-				cookie.setMaxAge(0); // 쿠키 삭제
-				response.addCookie(cookie);
+			
+			if(result == 1 ){	
+				HttpSession session = request.getSession();
+				session.setAttribute("id", id);
+				session.setAttribute("type", login_type);
+				
+				if(rememberId) {
+					//쿠키생성, 저장
+					Cookie cookie = new Cookie("id",id);
+					response.addCookie(cookie);
+				} else {
+					//쿠키삭제
+					Cookie cookie = new Cookie("id",id);
+					cookie.setMaxAge(0); // 쿠키 삭제
+					response.addCookie(cookie);
+				}
+				return "redirect:index";		// redirect 가 아닌  return "index"  로 보내게 되면 화면은 index로 가지만 주소창엔 login으로 남아있음
+			} else if (result == 0){
+				PrintWriter script = response.getWriter();
+				script.println("<script>alert('비밀번호가 틀립니다.'); history.back(); </script>");
+			} else if (result == -1){
+				PrintWriter script = response.getWriter();
+				script.println("<script>alert('존재하지 않는 아이디입니다.'); history.back(); </script>");
+			} else if (result == -2){
+				PrintWriter script = response.getWriter();
+				script.println("<script>alert('DB 오류'); history.back(); </script>");
 			}
-			return "redirect:index";		// redirect 가 아닌  return "index"  로 보내게 되면 화면은 index로 가지만 주소창엔 login으로 남아있음
-		} else if (result == 0){
-			PrintWriter script = response.getWriter();
-			script.println("<script>alert('비밀번호가 틀립니다.'); history.back(); </script>");
-		} else if (result == -1){
-			PrintWriter script = response.getWriter();
-			script.println("<script>alert('존재하지 않는 아이디입니다.'); history.back(); </script>");
-		} else if (result == -2){
-			PrintWriter script = response.getWriter();
-			script.println("<script>alert('DB 오류'); history.back(); </script>");
-		}
+		
+		
 		
 		return null;
 	}
@@ -78,7 +86,7 @@ public class LoginController {
 		
 		PrintWriter script = response.getWriter();
 		script.println("<script>alert('로그아웃 되었습니다.'); </script>");
-		script.println("<script>location.href = '/koala/index'; </script>");
+		script.println("<script>location.href = '/koalass/index'; </script>");
 		script.flush();	// 얘 하니까 안뜨던 alert가 뜨지만 return이 안먹어서 location.hrtf로 보냄..
 
 		return null;
