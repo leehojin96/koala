1fb9be1fcdb6ab72c709d096e419d657f123bf71
leehojin96 <st2035@naver.com>
2022-12-28 20:35:43 +0900

    준서님 파일 합본

---
+++ src/main/java/com/acorn/koala/Controller/KoalaController.java
@@ -0,0 +1,29 @@
+package com.acorn.koala.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class KoalaController {
+
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String index( HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		
+		String userid = (String) session.getAttribute("id");
+		String logintype = (String) session.getAttribute("type");
+
+		
+		request.setAttribute("userid", userid);
+		request.setAttribute("logintype", logintype);
+		return "index";
+	}
+	
+	
+
+	
+
+}
